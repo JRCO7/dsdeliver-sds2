6af1d6d00c68ded9f368fbf3f942b3df0b789f32
Joana Oliveira <joana7_yzfr6@hotmail.com>
2021-01-10 13:31:56 +0000

    Search Order location

--- front-web/src/Orders/StepsHeader.tsx
+++ front-web/src/Orders/StepsHeader.tsx
@@ -1,36 +1,35 @@
-import './styles.css';
 
 function StepsHeader() {
 
     return (
-      <header className = "orders-steps-container">
-<div className = "orders-steps-content">
-    <h1 className = "steps-title">
-SIGA AS <br/> ETAPAS
+        <header className="orders-steps-container">
+            <div className="orders-steps-content">
+                <h1 className="steps-title">
+                    SIGA AS <br /> ETAPAS
 
     </h1>
-    <ul className= "steps-items">
-        <li>
-<span className="steps-number">
+                <ul className="steps-items">
+                    <li>
+                        <span className="steps-number">
 
-1
+                            1
 </span>
 Selecione os produtos e localização
         </li>
-<li>
-<span className="steps-number">
+                    <li>
+                        <span className="steps-number">
 
-2
+                            2
 </span>
 Depois clique em <strong>"ENVIAR PEDIDO"</strong>
-</li>
+                    </li>
 
-    </ul>
-     </div>
+                </ul>
+            </div>
 
-      </header>
-        
-        )
+        </header>
+
+    )
 }
 
 export default StepsHeader;
